3071e48a4902fb482c0f3b36f627f3f16330d662
LorenzHW <lorenzhofmann.w@outlook.com>
2018-07-23 19:42:30 +0200

    authorization inside custom yes intent

--- src/skill/intents/yes_no_intents.py
+++ src/skill/intents/yes_no_intents.py
@@ -5,13 +5,15 @@ from ask_sdk_model.dialog import ElicitSlotDirective
 
 from src.skill.i18n.language_model import LanguageModel
 from src.skill.intents.message_intent import MessageIntentHandler
+from src.skill.services.daily_telegrams_service import DailyTelegramsService
 from src.skill.services.telethon_service import TelethonService
-from src.skill.utils.utils import send_telegram
+from src.skill.utils.utils import send_telegram, handle_authorization
 
 
 class YesIntentHandler(AbstractRequestHandler):
     def __init__(self):
         self.telethon_service = TelethonService()
+        self.daily_telegrams_service = DailyTelegramsService()
 
     def can_handle(self, handler_input):
         return is_intent_name("CustomYesIntent")(handler_input)
@@ -19,21 +21,33 @@ class YesIntentHandler(AbstractRequestHandler):
     def handle(self, handler_input):
         sess_attrs = handler_input.attributes_manager.session_attributes
         previous_intent = sess_attrs.get("PREV_INTENT")
+        user_is_authorized = sess_attrs.get("ACCOUNT").get("AUTHORIZED")
+        slots = handler_input.request_envelope.request.intent.slots
+
+        # User answered Yes on question: "Welcome, do you want to hear your new Telegrams?"
+        if previous_intent == "LaunchIntent" and user_is_authorized:
+            speech_text = MessageIntentHandler().get_telegram(handler_input)
+            handler_input.response_builder.speak(speech_text).set_should_end_session(False)
+            return handler_input.response_builder.response
+
+        # User answered Yes on question: "Welcome, u r not authorized. Authorize now?"
+        if (previous_intent == "LaunchIntent" or slots.get("code")) \
+                and not user_is_authorized:
+            handler_input = handle_authorization(handler_input)
+            return handler_input.response_builder.response
 
         # User answered Yes on question: "Do you want to reply?"
-        if previous_intent == "CustomYesIntent" or previous_intent == "AMAZON.NoIntent" and \
+        if (previous_intent == "CustomYesIntent" or previous_intent == "AMAZON.NoIntent") and \
                 sess_attrs.get("TELEGRAMS"):
-            slots = handler_input.request_envelope.request.intent.slots
 
             if slots.get("message").value:
-
                 contact = sess_attrs.get("CONTACTS")[sess_attrs.get("TELEGRAMS_COUNTER") - 1]
                 send_telegram(contact)
                 next_telegram = MessageIntentHandler().get_telegram(handler_input)
                 speech_text = "Telegram was sent to {}. ".format(contact) + next_telegram
             else:
-                updated_intent = Intent("CustomYesIntent", slots)
                 speech_text = "Ok, what is the message?"
+                updated_intent = Intent("CustomYesIntent", slots)
                 elicit_directive = ElicitSlotDirective(updated_intent, "message")
                 handler_input.response_builder.add_directive(elicit_directive)
 
@@ -41,15 +55,11 @@ class YesIntentHandler(AbstractRequestHandler):
             return handler_input.response_builder.response
 
         # User answered Yes on question: "Is there anything else I can help you with?"
-        if previous_intent == "SendIntent" or previous_intent == "CustomYesIntent" or previous_intent == "AMAZON.NoIntent" \
+        if (previous_intent == "SendIntent"
+            or previous_intent == "CustomYesIntent"
+            or previous_intent == "AMAZON.NoIntent") \
                 and not sess_attrs.get("TELEGRAMS"):
             speech_text = "I can help you to send a Telegram or check for new Telegrams. So, which do you need?"
-            handler_input.response_builder.speak(speech_text).set_should_end_session(False)
-            return handler_input.response_builder.response
-
-        # User answered Yes on question: "Welcome, do you want to hear your new Telegrams?"
-        if previous_intent == "LaunchIntent":
-            speech_text = MessageIntentHandler().get_telegram(handler_input)
             handler_input.response_builder.speak(speech_text).set_should_end_session(False)
             return handler_input.response_builder.response
 
@@ -65,6 +75,18 @@ class NoIntentHandler(AbstractRequestHandler):
         i18n = LanguageModel(handler_input.request_envelope.request.locale)
         sess_attrs = handler_input.attributes_manager.session_attributes
         previous_intent = sess_attrs.get("PREV_INTENT")
+        user_is_authorized = sess_attrs.get("ACCOUNT").get("AUTHORIZED")
+
+        # User answered Yes on question: "Welcome, do you want to hear your new Telegrams?"
+        if previous_intent == "LaunchIntent" and user_is_authorized:
+            speech_text = i18n.get_random_ack() + ", I can help you to send a Telegram or check for new Telegrams. So, which do you need?"
+            handler_input.response_builder.speak(speech_text).set_should_end_session(False)
+
+        # User answered Yes on question: "Welcome, u r not authorized. Authorize now?"
+        if previous_intent == "LaunchIntent" and not user_is_authorized:
+            speech_text = i18n.get_random_ack() + ", Bye for now"
+            handler_input.response_builder.speak(speech_text).set_should_end_session(True)
+            return handler_input.response_builder.response
 
         if previous_intent == "SendIntent":
             speech_text = i18n.get_random_ack() + ", " + i18n.get_random_goodbye()
@@ -77,15 +99,13 @@ class NoIntentHandler(AbstractRequestHandler):
             handler_input.response_builder.speak(speech_text).set_should_end_session(False)
             return handler_input.response_builder.response
 
-        # User answered Yes on question: "Is there anything else I can help you with?
-        if previous_intent == "CustomYesIntent" or previous_intent == "AMAZON.NoIntent" \
+        # User answered No on question: "Is there anything else I can help you with?
+        if (previous_intent == "SendIntent"
+            or previous_intent == "CustomYesIntent"
+            or previous_intent == "AMAZON.NoIntent") \
                 and not sess_attrs.get("TELEGRAMS"):
             speech_text = i18n.get_random_ack() + ", " + i18n.get_random_goodbye()
             handler_input.response_builder.speak(speech_text).set_should_end_session(True)
             return handler_input.response_builder.response
-
-        if previous_intent == "LaunchIntent":
-            speech_text = i18n.get_random_ack() + ", I can help you to send a Telegram or check for new Telegrams. So, which do you need?"
-            handler_input.response_builder.speak(speech_text).set_should_end_session(False)
 
         return handler_input.response_builder.response
--- src/skill/utils/utils.py
+++ src/skill/utils/utils.py
@@ -1,10 +1,15 @@
 from difflib import SequenceMatcher
 from html.parser import HTMLParser
 
+from ask_sdk_model import Intent
+from ask_sdk_model.dialog import ElicitSlotDirective
 from six import PY3
 
-
 ############## PARSER ##############
+from src.skill.i18n.language_model import LanguageModel
+from src.skill.services.telethon_service import TelethonService
+
+
 def convert_speech_to_text(ssml_speech):
     # convert ssml speech to text, by removing html tags
     s = SSMLStripper()
@@ -53,3 +58,41 @@ def send_telegram(first_name):
     print("SEND_TELEGRAM CONTACT")
     print(first_name)
     pass
+
+
+def handle_authorization(handler_input):
+    i18n = LanguageModel(handler_input.request_envelope.request.locale)
+    telethon_service = TelethonService()
+    sess_attrs = handler_input.attributes_manager.session_attributes
+    account = sess_attrs.get("ACCOUNT")
+    should_end = True
+    slots = handler_input.request_envelope.request.intent.slots
+
+    if not account.get("PHONE_NUMBER"):
+        speech_text = "You have not added a telephone number. Visit the website mentioned in the skill description and add a telephone number then try again. Bye for now"
+    elif not slots.get("code").value:
+        telethon_service.send_code_request()
+
+        updated_intent = Intent("CustomYesIntent", slots)
+        elicit_directive = ElicitSlotDirective(updated_intent, "code")
+        handler_input.response_builder.add_directive(elicit_directive)
+
+        speech_text = "You received a code on your phone. <break time='200ms' /> What is the code?"
+        should_end = False
+    else:
+        ok = telethon_service.sign_user_in(slots.get("code").value)
+
+        if ok:
+            sess_attrs["ACCOUNT"]["AUTHORIZED"] = True
+            speech_text = "Done. You are now authorized. <break time='200ms'/> I can help you send a Telegram or check for new Telegrams. So, which do you need?"
+            should_end = False
+        else:
+            speech_text = "The code is wrong. Please try again. Bye for now"
+
+    handler_input.response_builder.speak(speech_text).set_should_end_session(should_end)
+    return handler_input
+
+
+class BackendException(Exception):
+    def __init__(self, message):
+        super(BackendException, self).__init__(message)
