08394e00a5305ed6c17d02b3271cf0fc9c58a01d
LorenzHW <lorenzhofmann.w@gmail.com>
2019-01-20 04:46:49 -0500

    removing custom yes intent

--- lambda/us-east-1_myTelegrams/src/skill/intents/yes_no_intents.py
+++ lambda/us-east-1_myTelegrams/src/skill/intents/yes_no_intents.py
@@ -19,7 +19,7 @@ class YesIntentHandler(AbstractRequestHandler):
         sess_attrs = handler_input.attributes_manager.session_attributes
         # If Alexa is user listening for message slot on SendIntent it happens that she hears
         # 'yes' and therefore executes this intent handler. We don't want that.
-        return is_intent_name("CustomYesIntent")(handler_input) and not sess_attrs.get(
+        return is_intent_name("AMAZON.YesIntent")(handler_input) and not sess_attrs.get(
             "TELETHON_ENTITY_ID")
 
     def handle(self, handler_input):
@@ -27,7 +27,6 @@ class YesIntentHandler(AbstractRequestHandler):
         sess_attrs = handler_input.attributes_manager.session_attributes
         previous_intent = sess_attrs.get("PREV_INTENT")
         user_is_authorized = sess_attrs.get("ACCOUNT").get("AUTHORIZED")
-        slots = handler_input.request_envelope.request.intent.slots
 
         # User answered Yes on question: "Welcome, do you want to hear your new Telegrams?"
         if previous_intent == "LaunchIntent" and user_is_authorized:
@@ -36,42 +35,11 @@ class YesIntentHandler(AbstractRequestHandler):
                 .set_should_end_session(False).ask(i18n.FALLBACK)
             return handler_input.response_builder.response
 
-        # User answered Yes on question: "Welcome, u r not authorized. Authorize now?"
-        if (previous_intent == "LaunchIntent" or slots.get("code")) \
-                and not user_is_authorized:
-            handler_input = handle_authorization(handler_input)
-            return handler_input.response_builder.response
-
-        # User answered Yes on question: "Do you want to reply?"
-        if (previous_intent == "CustomYesIntent"
-            or previous_intent == "AMAZON.NoIntent"
-            or previous_intent == "MessageIntent") \
-                and sess_attrs.get("TELEGRAMS"):
-
-            if slots.get("message").value:
-                contact = sess_attrs.get("CONTACTS")[sess_attrs.get("TELEGRAMS_COUNTER") - 1]
-                entity_id = sess_attrs.get("ENTITY_IDS")[sess_attrs.get("TELEGRAMS_COUNTER") - 1]
-                self.telethon_service.send_telegram(entity_id, slots.get("message").value)
-
-                next_telegram = MessageIntentHandler().get_telegram(handler_input)
-                speech_text = i18n.TELEGRAM_SENT.format(contact) + next_telegram
-                reprompt = i18n.FALLBACK
-            else:
-                speech_text = i18n.get_random_acceptance_ack() + ", " + i18n.MESSAGE_2
-                reprompt = i18n.get_random_dont_understand() + ", " + i18n.MESSAGE_2
-                updated_intent = Intent("CustomYesIntent", slots)
-                elicit_directive = ElicitSlotDirective(updated_intent, "message")
-                handler_input.response_builder.add_directive(elicit_directive)
-
-            handler_input.response_builder.speak(speech_text) \
-                .set_should_end_session(False).ask(reprompt)
-            return handler_input.response_builder.response
-
         # User answered Yes on question: "Is there anything else I can help you with?"
         if (previous_intent == "SendIntent"
             or previous_intent == "MessageIntent"
             or previous_intent == "SpeedIntent"
-            or previous_intent == "CustomYesIntent"
+            or previous_intent == "AMAZON.YesIntent"
             or previous_intent == "AMAZON.NoIntent") \
                 and not sess_attrs.get("TELEGRAMS") and not sess_attrs.get("FIRST_NAMES"):
             speech_text = i18n.HELP_USER
@@ -79,14 +47,6 @@ class YesIntentHandler(AbstractRequestHandler):
             handler_input.response_builder.speak(speech_text) \
                 .set_should_end_session(False).ask(reprompt)
             return handler_input.response_builder.response
-
-        # User answered with not a number when Alexa is suggesting contacts in SendIntent
-        if (previous_intent == "SendIntent" and sess_attrs.get("FIRST_NAMES")):
-            speech_text = i18n.MAX_NO_CONTACT
-            handler_input.response_builder.speak(speech_text) \
-                .set_should_end_session(True)
-            return handler_input.response_builder.response
-
 
 class NoIntentHandler(AbstractRequestHandler):
     def __init__(self):
@@ -108,22 +68,6 @@ class NoIntentHandler(AbstractRequestHandler):
         # User answered No on question: "Welcome, do you want to hear your new Telegrams?"
         if previous_intent == "LaunchIntent" and user_is_authorized:
             speech_text = i18n.get_random_ack() + ", " + i18n.HELP_USER
-            handler_input.response_builder.speak(speech_text) \
-                .set_should_end_session(False).ask(i18n.FALLBACK)
-            return handler_input.response_builder.response
-
-        # User answered No on question: "Welcome, u r not authorized. Authorize now?"
-        if previous_intent == "LaunchIntent" and not user_is_authorized:
-            speech_text = i18n.get_random_ack() + ", " + i18n.BYE_FOR_NOW
-            handler_input.response_builder.speak(speech_text).set_should_end_session(True)
-            return handler_input.response_builder.response
-
-        # User answered No on the question if he wants to reply
-        if (previous_intent == "CustomYesIntent"
-            or previous_intent == "AMAZON.NoIntent"
-            or previous_intent == "MessageIntent") and \
-                sess_attrs.get("TELEGRAMS"):
-            speech_text = MessageIntentHandler().get_telegram(handler_input)
             handler_input.response_builder.speak(speech_text) \
                 .set_should_end_session(False).ask(i18n.FALLBACK)
             return handler_input.response_builder.response
