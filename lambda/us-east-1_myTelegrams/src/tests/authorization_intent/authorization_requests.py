56a420250fbf107740d87b77ae65245212dfcef8
LorenzHW <lorenzhofmann.w@gmail.com>
2019-01-22 16:30:37 -0500

    authorization test only if no phone number

--- lambda/us-east-1_myTelegrams/src/tests/authorization_intent/authorization_requests.py
+++ lambda/us-east-1_myTelegrams/src/tests/authorization_intent/authorization_requests.py
@@ -1,229 +1,76 @@
-authorization_request_1 = {
-    "version": "1.0",
-    "session": {
-        "new": False,
-        "sessionId": "amzn1.echo-api.session.e0908784-ffc7-4eb8-9c5b-e736d9dfc6a3",
-        "application": {
-            "applicationId": "amzn1.ask.skill.c171fbe5-e5b6-47f0-88ea-d0cc10a9d0f0"
-        },
-        "attributes": {
-            "PREV_INTENT": "LaunchIntent",
-            "ACCOUNT": {
-                "ID": "416",
-                "PHONE_NUMBER": "",
-                "AUTHORIZED": False
-            }
-        },
-        "user": {
-            "userId": "amzn1.ask.account.AFA3NBQCSUS4IPO2GDFE674DXFQ64OKO2OKJPX67T77N6RZNJ3QVVVBECH6JJCS42ELLFNDZEM2FANIZT6EUZZ5VERBPKK5CGJH3LF235XY5SZAXYDOFDVK425JEEHXBZTUL2JOF5CVVOZJQCLBL5YDIB3BIVHUSABN3LHWENAPU7CEHG2I62TE43I43WODVWHTQDGKQ5MYGBLY",
-            "accessToken": "o7zXYvr1vQcT5NcPOf4wWaYm9ErB7O"
-        }
-    },
-    "context": {
-        "System": {
-            "application": {
-                "applicationId": "amzn1.ask.skill.c171fbe5-e5b6-47f0-88ea-d0cc10a9d0f0"
-            },
-            "user": {
-                "userId": "amzn1.ask.account.AFA3NBQCSUS4IPO2GDFE674DXFQ64OKO2OKJPX67T77N6RZNJ3QVVVBECH6JJCS42ELLFNDZEM2FANIZT6EUZZ5VERBPKK5CGJH3LF235XY5SZAXYDOFDVK425JEEHXBZTUL2JOF5CVVOZJQCLBL5YDIB3BIVHUSABN3LHWENAPU7CEHG2I62TE43I43WODVWHTQDGKQ5MYGBLY",
-                "accessToken": "o7zXYvr1vQcT5NcPOf4wWaYm9ErB7O"
-            },
-            "device": {
-                "deviceId": "amzn1.ask.device.AEVW24XQJRQBDBTQ4T3OYGSB2NGIGNIEILGIV2FSLEPC5I5BYYOU47V7G2XEF4L4L2PRMUJCSQHKPSDSCQI2PF73YRIWGXM23AOHPPKAZRLWDQAAMAW4ANLWXSWIZKMSIMAK32LR572AOVVFFKUSX7VRC27A",
-                "supportedInterfaces": {}
-            },
-            "apiEndpoint": "https://api.amazonalexa.com",
-            "apiAccessToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjEifQ.eyJhdWQiOiJodHRwczovL2FwaS5hbWF6b25hbGV4YS5jb20iLCJpc3MiOiJBbGV4YVNraWxsS2l0Iiwic3ViIjoiYW16bjEuYXNrLnNraWxsLmMxNzFmYmU1LWU1YjYtNDdmMC04OGVhLWQwY2MxMGE5ZDBmMCIsImV4cCI6MTU0NzkwNjgwMywiaWF0IjoxNTQ3OTAzMjAzLCJuYmYiOjE1NDc5MDMyMDMsInByaXZhdGVDbGFpbXMiOnsiY29uc2VudFRva2VuIjpudWxsLCJkZXZpY2VJZCI6ImFtem4xLmFzay5kZXZpY2UuQUVWVzI0WFFKUlFCREJUUTRUM09ZR1NCMk5HSUdOSUVJTEdJVjJGU0xFUEM1STVCWVlPVTQ3VjdHMlhFRjRMNEwyUFJNVUpDU1FIS1BTRFNDUUkyUEY3M1lSSVdHWE0yM0FPSFBQS0FaUkxXRFFBQU1BVzRBTkxXWFNXSVpLTVNJTUFLMzJMUjU3MkFPVlZGRktVU1g3VlJDMjdBIiwidXNlcklkIjoiYW16bjEuYXNrLmFjY291bnQuQUZBM05CUUNTVVM0SVBPMkdERkU2NzREWEZRNjRPS08yT0tKUFg2N1Q3N042UlpOSjNRVlZWQkVDSDZKSkNTNDJFTExGTkRaRU0yRkFOSVpUNkVVWlo1VkVSQlBLSzVDR0pIM0xGMjM1WFk1U1pBWFlET0ZEVks0MjVKRUVIWEJaVFVMMkpPRjVDVlZPWkpRQ0xCTDVZRElCM0JJVkhVU0FCTjNMSFdFTkFQVTdDRUhHMkk2MlRFNDNJNDNXT0RWV0hUUURHS1E1TVlHQkxZIn19.Sz8681y8NxGOwbUJPS6nYUa1YQOl3n5--modhtjjbRxiEEyXr-0f6C2GabuMs_hQWG1vYarGURCXdg7hDc1zX_95Gkq7NOlqbII4nLhgO5yXvuYlsve-bJtvpROWk5KmGCEuP0-UsGAzGxX0cOUf-hQN1sDHvWFwaa90IQCIUoW9zU7DK5nxZkQybHzg1b0LC_AU66pVNECeCKGEJDBJexr43kYhu70RG2Qy6D3AqvDA9Nx7fJvKVMAS9PNYtoEGAV2VTVrEzvXw8qMCDecj_u7m-2JF1hXpFk0npURd6aFoq0prSUtDoM7QoeXEw_2PhHrmf6p6-M_95F4VxApDAg"
-        },
-        "Viewport": {
-            "experiences": [
-                    {
-                        "arcMinuteWidth": 246,
-                        "arcMinuteHeight": 144,
-                        "canRotate": False,
-                        "canResize": False
-                    }
-            ],
-            "shape": "RECTANGLE",
-            "pixelWidth": 1024,
-            "pixelHeight": 600,
-            "dpi": 160,
-            "currentPixelWidth": 1024,
-            "currentPixelHeight": 600,
-            "touch": [
-                "SINGLE"
-            ]
-        }
-    },
-    "request": {
-        "type": "IntentRequest",
-        "requestId": "amzn1.echo-api.request.994dba33-160d-4da6-83ab-0a3ecac43436",
-        "timestamp": "2019-01-19T13:06:43Z",
-                "locale": "en-GB",
-                "intent": {
-                    "name": "AuthorizationIntent",
-                    "confirmationStatus": "NONE",
-                    "slots": {
-                            "code": {
-                                "name": "code",
-                                "confirmationStatus": "NONE"
-                            }
-                    }
-                },
-        "dialogState": "STARTED"
-    }
-}
-
-authorization_request_2 = {
-    "version": "1.0",
-    "session": {
-        "new": False,
-        "sessionId": "amzn1.echo-api.session.e0908784-ffc7-4eb8-9c5b-e736d9dfc6a3",
-        "application": {
-            "applicationId": "amzn1.ask.skill.c171fbe5-e5b6-47f0-88ea-d0cc10a9d0f0"
-        },
-        "attributes": {
-            "PREV_INTENT": "LaunchIntent",
-            "ACCOUNT": {
-                "ID": "416",
-                "PHONE_NUMBER": "+4366488715585",
-                "AUTHORIZED": False
-            }
-        },
-        "user": {
-            "userId": "amzn1.ask.account.AFA3NBQCSUS4IPO2GDFE674DXFQ64OKO2OKJPX67T77N6RZNJ3QVVVBECH6JJCS42ELLFNDZEM2FANIZT6EUZZ5VERBPKK5CGJH3LF235XY5SZAXYDOFDVK425JEEHXBZTUL2JOF5CVVOZJQCLBL5YDIB3BIVHUSABN3LHWENAPU7CEHG2I62TE43I43WODVWHTQDGKQ5MYGBLY",
-            "accessToken": "o7zXYvr1vQcT5NcPOf4wWaYm9ErB7O"
-        }
-    },
-    "context": {
-        "System": {
-            "application": {
-                "applicationId": "amzn1.ask.skill.c171fbe5-e5b6-47f0-88ea-d0cc10a9d0f0"
-            },
-            "user": {
-                "userId": "amzn1.ask.account.AFA3NBQCSUS4IPO2GDFE674DXFQ64OKO2OKJPX67T77N6RZNJ3QVVVBECH6JJCS42ELLFNDZEM2FANIZT6EUZZ5VERBPKK5CGJH3LF235XY5SZAXYDOFDVK425JEEHXBZTUL2JOF5CVVOZJQCLBL5YDIB3BIVHUSABN3LHWENAPU7CEHG2I62TE43I43WODVWHTQDGKQ5MYGBLY",
-                "accessToken": "o7zXYvr1vQcT5NcPOf4wWaYm9ErB7O"
-            },
-            "device": {
-                "deviceId": "amzn1.ask.device.AEVW24XQJRQBDBTQ4T3OYGSB2NGIGNIEILGIV2FSLEPC5I5BYYOU47V7G2XEF4L4L2PRMUJCSQHKPSDSCQI2PF73YRIWGXM23AOHPPKAZRLWDQAAMAW4ANLWXSWIZKMSIMAK32LR572AOVVFFKUSX7VRC27A",
-                "supportedInterfaces": {}
-            },
-            "apiEndpoint": "https://api.amazonalexa.com",
-            "apiAccessToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjEifQ.eyJhdWQiOiJodHRwczovL2FwaS5hbWF6b25hbGV4YS5jb20iLCJpc3MiOiJBbGV4YVNraWxsS2l0Iiwic3ViIjoiYW16bjEuYXNrLnNraWxsLmMxNzFmYmU1LWU1YjYtNDdmMC04OGVhLWQwY2MxMGE5ZDBmMCIsImV4cCI6MTU0NzkwNjgwMywiaWF0IjoxNTQ3OTAzMjAzLCJuYmYiOjE1NDc5MDMyMDMsInByaXZhdGVDbGFpbXMiOnsiY29uc2VudFRva2VuIjpudWxsLCJkZXZpY2VJZCI6ImFtem4xLmFzay5kZXZpY2UuQUVWVzI0WFFKUlFCREJUUTRUM09ZR1NCMk5HSUdOSUVJTEdJVjJGU0xFUEM1STVCWVlPVTQ3VjdHMlhFRjRMNEwyUFJNVUpDU1FIS1BTRFNDUUkyUEY3M1lSSVdHWE0yM0FPSFBQS0FaUkxXRFFBQU1BVzRBTkxXWFNXSVpLTVNJTUFLMzJMUjU3MkFPVlZGRktVU1g3VlJDMjdBIiwidXNlcklkIjoiYW16bjEuYXNrLmFjY291bnQuQUZBM05CUUNTVVM0SVBPMkdERkU2NzREWEZRNjRPS08yT0tKUFg2N1Q3N042UlpOSjNRVlZWQkVDSDZKSkNTNDJFTExGTkRaRU0yRkFOSVpUNkVVWlo1VkVSQlBLSzVDR0pIM0xGMjM1WFk1U1pBWFlET0ZEVks0MjVKRUVIWEJaVFVMMkpPRjVDVlZPWkpRQ0xCTDVZRElCM0JJVkhVU0FCTjNMSFdFTkFQVTdDRUhHMkk2MlRFNDNJNDNXT0RWV0hUUURHS1E1TVlHQkxZIn19.Sz8681y8NxGOwbUJPS6nYUa1YQOl3n5--modhtjjbRxiEEyXr-0f6C2GabuMs_hQWG1vYarGURCXdg7hDc1zX_95Gkq7NOlqbII4nLhgO5yXvuYlsve-bJtvpROWk5KmGCEuP0-UsGAzGxX0cOUf-hQN1sDHvWFwaa90IQCIUoW9zU7DK5nxZkQybHzg1b0LC_AU66pVNECeCKGEJDBJexr43kYhu70RG2Qy6D3AqvDA9Nx7fJvKVMAS9PNYtoEGAV2VTVrEzvXw8qMCDecj_u7m-2JF1hXpFk0npURd6aFoq0prSUtDoM7QoeXEw_2PhHrmf6p6-M_95F4VxApDAg"
-        },
-        "Viewport": {
-            "experiences": [
-                    {
-                        "arcMinuteWidth": 246,
-                        "arcMinuteHeight": 144,
-                        "canRotate": False,
-                        "canResize": False
-                    }
-            ],
-            "shape": "RECTANGLE",
-            "pixelWidth": 1024,
-            "pixelHeight": 600,
-            "dpi": 160,
-            "currentPixelWidth": 1024,
-            "currentPixelHeight": 600,
-            "touch": [
-                "SINGLE"
-            ]
-        }
-    },
-    "request": {
-        "type": "IntentRequest",
-        "requestId": "amzn1.echo-api.request.994dba33-160d-4da6-83ab-0a3ecac43436",
-        "timestamp": "2019-01-19T13:06:43Z",
-                "locale": "en-GB",
-                "intent": {
-                    "name": "AuthorizationIntent",
-                    "confirmationStatus": "NONE",
-                    "slots": {
-                            "code": {
-                                "name": "code",
-                                "confirmationStatus": "NONE"
-                            }
-                    }
-                },
-        "dialogState": "STARTED"
-    }
-}
-
-
-authorization_request_3 = {
-    "version": "1.0",
-    "session": {
-        "new": False,
-        "sessionId": "amzn1.echo-api.session.e0908784-ffc7-4eb8-9c5b-e736d9dfc6a3",
-        "application": {
-            "applicationId": "amzn1.ask.skill.c171fbe5-e5b6-47f0-88ea-d0cc10a9d0f0"
-        },
-        "attributes": {
-            "PREV_INTENT": "LaunchIntent",
-            "ACCOUNT": {
-                "ID": "416",
-                "PHONE_NUMBER": "+4366488715585",
-                "AUTHORIZED": False
-            }
-        },
-        "user": {
-            "userId": "amzn1.ask.account.AFA3NBQCSUS4IPO2GDFE674DXFQ64OKO2OKJPX67T77N6RZNJ3QVVVBECH6JJCS42ELLFNDZEM2FANIZT6EUZZ5VERBPKK5CGJH3LF235XY5SZAXYDOFDVK425JEEHXBZTUL2JOF5CVVOZJQCLBL5YDIB3BIVHUSABN3LHWENAPU7CEHG2I62TE43I43WODVWHTQDGKQ5MYGBLY",
-            "accessToken": "o7zXYvr1vQcT5NcPOf4wWaYm9ErB7O"
-        }
-    },
-    "context": {
-        "System": {
-            "application": {
-                "applicationId": "amzn1.ask.skill.c171fbe5-e5b6-47f0-88ea-d0cc10a9d0f0"
-            },
-            "user": {
-                "userId": "amzn1.ask.account.AFA3NBQCSUS4IPO2GDFE674DXFQ64OKO2OKJPX67T77N6RZNJ3QVVVBECH6JJCS42ELLFNDZEM2FANIZT6EUZZ5VERBPKK5CGJH3LF235XY5SZAXYDOFDVK425JEEHXBZTUL2JOF5CVVOZJQCLBL5YDIB3BIVHUSABN3LHWENAPU7CEHG2I62TE43I43WODVWHTQDGKQ5MYGBLY",
-                "accessToken": "o7zXYvr1vQcT5NcPOf4wWaYm9ErB7O"
-            },
-            "device": {
-                "deviceId": "amzn1.ask.device.AEVW24XQJRQBDBTQ4T3OYGSB2NGIGNIEILGIV2FSLEPC5I5BYYOU47V7G2XEF4L4L2PRMUJCSQHKPSDSCQI2PF73YRIWGXM23AOHPPKAZRLWDQAAMAW4ANLWXSWIZKMSIMAK32LR572AOVVFFKUSX7VRC27A",
-                "supportedInterfaces": {}
-            },
-            "apiEndpoint": "https://api.amazonalexa.com",
-            "apiAccessToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjEifQ.eyJhdWQiOiJodHRwczovL2FwaS5hbWF6b25hbGV4YS5jb20iLCJpc3MiOiJBbGV4YVNraWxsS2l0Iiwic3ViIjoiYW16bjEuYXNrLnNraWxsLmMxNzFmYmU1LWU1YjYtNDdmMC04OGVhLWQwY2MxMGE5ZDBmMCIsImV4cCI6MTU0NzkwNjgwMywiaWF0IjoxNTQ3OTAzMjAzLCJuYmYiOjE1NDc5MDMyMDMsInByaXZhdGVDbGFpbXMiOnsiY29uc2VudFRva2VuIjpudWxsLCJkZXZpY2VJZCI6ImFtem4xLmFzay5kZXZpY2UuQUVWVzI0WFFKUlFCREJUUTRUM09ZR1NCMk5HSUdOSUVJTEdJVjJGU0xFUEM1STVCWVlPVTQ3VjdHMlhFRjRMNEwyUFJNVUpDU1FIS1BTRFNDUUkyUEY3M1lSSVdHWE0yM0FPSFBQS0FaUkxXRFFBQU1BVzRBTkxXWFNXSVpLTVNJTUFLMzJMUjU3MkFPVlZGRktVU1g3VlJDMjdBIiwidXNlcklkIjoiYW16bjEuYXNrLmFjY291bnQuQUZBM05CUUNTVVM0SVBPMkdERkU2NzREWEZRNjRPS08yT0tKUFg2N1Q3N042UlpOSjNRVlZWQkVDSDZKSkNTNDJFTExGTkRaRU0yRkFOSVpUNkVVWlo1VkVSQlBLSzVDR0pIM0xGMjM1WFk1U1pBWFlET0ZEVks0MjVKRUVIWEJaVFVMMkpPRjVDVlZPWkpRQ0xCTDVZRElCM0JJVkhVU0FCTjNMSFdFTkFQVTdDRUhHMkk2MlRFNDNJNDNXT0RWV0hUUURHS1E1TVlHQkxZIn19.Sz8681y8NxGOwbUJPS6nYUa1YQOl3n5--modhtjjbRxiEEyXr-0f6C2GabuMs_hQWG1vYarGURCXdg7hDc1zX_95Gkq7NOlqbII4nLhgO5yXvuYlsve-bJtvpROWk5KmGCEuP0-UsGAzGxX0cOUf-hQN1sDHvWFwaa90IQCIUoW9zU7DK5nxZkQybHzg1b0LC_AU66pVNECeCKGEJDBJexr43kYhu70RG2Qy6D3AqvDA9Nx7fJvKVMAS9PNYtoEGAV2VTVrEzvXw8qMCDecj_u7m-2JF1hXpFk0npURd6aFoq0prSUtDoM7QoeXEw_2PhHrmf6p6-M_95F4VxApDAg"
-        },
-        "Viewport": {
-            "experiences": [
-                    {
-                        "arcMinuteWidth": 246,
-                        "arcMinuteHeight": 144,
-                        "canRotate": False,
-                        "canResize": False
-                    }
-            ],
-            "shape": "RECTANGLE",
-            "pixelWidth": 1024,
-            "pixelHeight": 600,
-            "dpi": 160,
-            "currentPixelWidth": 1024,
-            "currentPixelHeight": 600,
-            "touch": [
-                "SINGLE"
-            ]
-        }
-    },
-    "request": {
-        "type": "IntentRequest",
-        "requestId": "amzn1.echo-api.request.994dba33-160d-4da6-83ab-0a3ecac43436",
-        "timestamp": "2019-01-19T13:06:43Z",
-                "locale": "en-GB",
-                "intent": {
-                    "name": "AuthorizationIntent",
-                    "confirmationStatus": "NONE",
-                    "slots": {
-                            "code": {
-                                "name": "code",
-                                "confirmationStatus": "NONE",
-                                "value": "123"
-                            }
-                    }
-                },
-        "dialogState": "STARTED"
-    }
+no_phone_added = {
+	"version": "1.0",
+	"session": {
+		"new": False,
+		"sessionId": "amzn1.echo-api.session.4b0e45f9-4870-468a-a02f-964f183dbba9",
+		"application": {
+			"applicationId": "amzn1.ask.skill.c171fbe5-e5b6-47f0-88ea-d0cc10a9d0f0"
+		},
+		"attributes": {
+			"PREV_INTENT": "AMAZON.HelpIntent",
+			"ACCOUNT": {
+				"ID": "420",
+				"PHONE_NUMBER": "",
+				"AUTHORIZED": False,
+				"SETTINGS_ID": None
+			}
+		},
+		"user": {
+			"userId": "amzn1.ask.account.AHZ74GVUPBM627GWU5KJ77IMTQY5P3EIQ53ZJEBIT7RKEXOT6LPOVZVSWAVT4FU2ADRKRH2S37D7J343GTLO3KMNB2NB7NFDFENJX32U4HJCTQUZ7PBWBPBBYV42NOAEQH2TZ3VZ2IYIDUMLIA6TIUQDRIZQU4PTQPDHNP6E66XDDPWAPOI6ERD5Y5I2GMZGQQET7SS5TPTDQGQ",
+			"accessToken": "t1wxq3J6QJEh7CitrtAIfc7i3LWQEC"
+		}
+	},
+	"context": {
+		"System": {
+			"application": {
+				"applicationId": "amzn1.ask.skill.c171fbe5-e5b6-47f0-88ea-d0cc10a9d0f0"
+			},
+			"user": {
+				"userId": "amzn1.ask.account.AHZ74GVUPBM627GWU5KJ77IMTQY5P3EIQ53ZJEBIT7RKEXOT6LPOVZVSWAVT4FU2ADRKRH2S37D7J343GTLO3KMNB2NB7NFDFENJX32U4HJCTQUZ7PBWBPBBYV42NOAEQH2TZ3VZ2IYIDUMLIA6TIUQDRIZQU4PTQPDHNP6E66XDDPWAPOI6ERD5Y5I2GMZGQQET7SS5TPTDQGQ",
+				"accessToken": "t1wxq3J6QJEh7CitrtAIfc7i3LWQEC"
+			},
+			"device": {
+				"deviceId": "amzn1.ask.device.AH32ZIERETTWPUS4NSIT7JMP5QTBNUHATO7HQDWLGLM6JR6E574C4RSFM67MWD33576JRSWHD3X5DPDMILHQN225AQRSOKB35S2GW74SDJHNN2A7M3QMMZLT4GWMITGRX6OASQSCXLMWWQ5VN2V4PN3KQFYA",
+				"supportedInterfaces": {}
+			},
+			"apiEndpoint": "https://api.amazonalexa.com",
+			"apiAccessToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjEifQ.eyJhdWQiOiJodHRwczovL2FwaS5hbWF6b25hbGV4YS5jb20iLCJpc3MiOiJBbGV4YVNraWxsS2l0Iiwic3ViIjoiYW16bjEuYXNrLnNraWxsLmMxNzFmYmU1LWU1YjYtNDdmMC04OGVhLWQwY2MxMGE5ZDBmMCIsImV4cCI6MTU0ODE5Mzk4MCwiaWF0IjoxNTQ4MTkwMzgwLCJuYmYiOjE1NDgxOTAzODAsInByaXZhdGVDbGFpbXMiOnsiY29uc2VudFRva2VuIjpudWxsLCJkZXZpY2VJZCI6ImFtem4xLmFzay5kZXZpY2UuQUgzMlpJRVJFVFRXUFVTNE5TSVQ3Sk1QNVFUQk5VSEFUTzdIUURXTEdMTTZKUjZFNTc0QzRSU0ZNNjdNV0QzMzU3NkpSU1dIRDNYNURQRE1JTEhRTjIyNUFRUlNPS0IzNVMyR1c3NFNESkhOTjJBN00zUU1NWkxUNEdXTUlUR1JYNk9BU1FTQ1hMTVdXUTVWTjJWNFBOM0tRRllBIiwidXNlcklkIjoiYW16bjEuYXNrLmFjY291bnQuQUhaNzRHVlVQQk02MjdHV1U1S0o3N0lNVFFZNVAzRUlRNTNaSkVCSVQ3UktFWE9UNkxQT1ZaVlNXQVZUNEZVMkFEUktSSDJTMzdEN0ozNDNHVExPM0tNTkIyTkI3TkZERkVOSlgzMlU0SEpDVFFVWjdQQldCUEJCWVY0Mk5PQUVRSDJUWjNWWjJJWUlEVU1MSUE2VElVUURSSVpRVTRQVFFQREhOUDZFNjZYRERQV0FQT0k2RVJENVk1STJHTVpHUVFFVDdTUzVUUFREUUdRIn19.JKoAotAe6EGQGWnus4uE_im81vlBow_l-0cOPNj_q33qZxisRxqZB3-B7AdxY6I8GOkj-3QhEs6pJQmkD3FaxsH-4zZsWExN_m0y9bA1frm0v2FVYWdzfSPYQbkWMczRoBTStE_Z_C7k9p0I2KSJhZ-tdMthbQyXm8L82v_fDD87W6JZY-zSYD7WBOHpvNnwbbYvrR_yISRjfRl4qADb1IUJsfqqHyTFN5jKE_3WV6ccHvzCGif8gLwZpTnkVWF8L11XelQv4Qz0mTwySv8SP_ojWxyQfpF06KvM-Rk43wdi5c87QBglOFvwOTr8EKZ-V06y7n06HTOacBaFoTLhoA"
+		},
+		"Viewport": {
+			"experiences": [
+				{
+					"arcMinuteWidth": 246,
+					"arcMinuteHeight": 144,
+					"canRotate": False,
+					"canResize": False
+				}
+			],
+			"shape": "RECTANGLE",
+			"pixelWidth": 1024,
+			"pixelHeight": 600,
+			"dpi": 160,
+			"currentPixelWidth": 1024,
+			"currentPixelHeight": 600,
+			"touch": [
+				"SINGLE"
+			]
+		}
+	},
+	"request": {
+		"type": "IntentRequest",
+		"requestId": "amzn1.echo-api.request.b0801e3d-4d22-4e2d-86f6-35dcbf60e0ff",
+		"timestamp": "2019-01-22T20:53:00Z",
+		"locale": "de-DE",
+		"intent": {
+			"name": "AuthorizationIntent",
+			"confirmationStatus": "NONE",
+			"slots": {
+				"code": {
+					"name": "code",
+					"confirmationStatus": "NONE"
+				}
+			}
+		},
+		"dialogState": "STARTED"
+	}
 }
--- lambda/us-east-1_myTelegrams/src/tests/authorization_intent/authorization_test.py
+++ lambda/us-east-1_myTelegrams/src/tests/authorization_intent/authorization_test.py
@@ -1,67 +1,26 @@
 
 import unittest
 
-from src.tests.authorization_intent.authorization_requests import authorization_request_1, authorization_request_2, authorization_request_3
-from src.tests.secret import VALID_TOKEN
-from src.skill.i18n.language_model import LanguageModel
+from src.tests.authorization_intent.authorization_requests import no_phone_added
+from src.tests.tokens import VALID_TOKEN
+from src.skill.i18n.non_verbose_language_model import NonVerboseLanguageModel
 from lambda_function import sb
 
 
 class AlexaParticleTests(unittest.TestCase):
     def authorization_with_no_phone(self):
-        i18n = LanguageModel('en-US')
+        i18n = NonVerboseLanguageModel('de-DE', False)
         handler = sb.lambda_handler()
 
         # User wants to authorize: user did not add phone number
-        authorization_request_1["context"]["System"]["user"]["accessToken"] = VALID_TOKEN
-        authorization_request_1["session"]["user"]["accessToken"] = VALID_TOKEN
-        event = handler(authorization_request_1, None)
+        no_phone_added["context"]["System"]["user"]["accessToken"] = VALID_TOKEN
+        no_phone_added["session"]["user"]["accessToken"] = VALID_TOKEN
+        event = handler(no_phone_added, None)
         ssml = event.get('response').get('outputSpeech').get('ssml')
         self.assertEqual(ssml, '<speak>{}</speak>'.format(i18n.NO_PHONE))
-
-    def send_code(self):
-        i18n = LanguageModel('en-US')
-        handler = sb.lambda_handler()
-
-        # User wants to authorize: either user receives code
-        # This breaks a lot, because we also send request into backend..
-        authorization_request_2["context"]["System"]["user"]["accessToken"] = VALID_TOKEN
-        authorization_request_2["session"]["user"]["accessToken"] = VALID_TOKEN
-        event = handler(authorization_request_2, None)
-        ssml = event.get('response').get('outputSpeech').get('ssml')
-        self.assertTrue(ssml in '<speak>{}</speak>'.format(i18n.WHAT_IS_CODE) or  ssml[:58] in '<speak>{}</speak>'.format(i18n.FLOODWAIT_ERROR))
-
-    def sign_in(self):
-        i18n = LanguageModel('en-US')
-        handler = sb.lambda_handler()
-        # Set manually
-        code = None
-        wrong_code = '17832'
-        authorization_request_3["context"]["System"]["user"]["accessToken"] = VALID_TOKEN
-        authorization_request_3["session"]["user"]["accessToken"] = VALID_TOKEN
-        
-        if code:
-            authorization_request_3["request"]["intent"]["slots"]["code"]["value"] = code
-            event = handler(authorization_request_3, None)
-            ssml = event.get('response').get('outputSpeech').get('ssml')
-            ssml = ssml[-127:]
-            self.assertTrue(ssml in i18n.AUTHORIZED)
-        elif wrong_code:
-            authorization_request_3["request"]["intent"]["slots"]["code"]["value"] = wrong_code
-            event = handler(authorization_request_3, None)
-            ssml = event.get('response').get('outputSpeech').get('ssml')
-            ssml = ssml[7:-8]
-            self.assertTrue(ssml in i18n.WRONG_CODE or ssml in i18n.CODE_EXPIRED)
-        else:
-            self.assertTrue(True)
-        
-        
-
 
 if __name__ == "__main__":
     suite = unittest.TestSuite()
     suite.addTest(AlexaParticleTests("authorization_with_no_phone"))
-    suite.addTest(AlexaParticleTests("send_code"))
-    suite.addTest(AlexaParticleTests("sign_in"))
     runner = unittest.TextTestRunner()
     runner.run(suite)
