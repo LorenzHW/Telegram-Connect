4c2fbb56949f143e59abef3409131401df7450e9
LorenzHW <lorenzhofmann.w@gmail.com>
2020-11-03 20:34:39 +0100

    Casting state attributes to native Python types, otherwise Pyrogram breaks

--- lambda/skill/state.py
+++ lambda/skill/state.py
@@ -32,9 +32,25 @@ class State:
         self.new_session_count = data.get("new_session_count", Decimal(0))
         self.dc_id = data.get('dc_id', Decimal(0))
         self.auth_key = data.get('auth_key')
-        if isinstance(self.auth_key, Binary):
-            self.auth_key = self.auth_key.value
         self.test_mode = data.get('test_mode')
         self.user_id = data.get('user_id', Decimal(0))
         self.is_bot = data.get('is_bot', False)
         self.peers = data.get('peers', [])
+
+        self._cast_to_native_python_types()
+
+    def _cast_to_native_python_types(self):
+        """
+        When we receive data from DynamoDB, we don't get native Python types. However, other libraries (e.g.: Pyrogram)
+        work only with native Python types. Therefore, we need to cast it.
+        """
+        if isinstance(self.user_id, Decimal):
+            self.user_id = int(self.user_id)
+
+        if isinstance(self.auth_key, Binary):
+            self.auth_key = self.auth_key.value
+
+        for p in self.peers:
+            for idx, element in enumerate(p):
+                if isinstance(element, Decimal):
+                    p[idx] = int(p[idx])
